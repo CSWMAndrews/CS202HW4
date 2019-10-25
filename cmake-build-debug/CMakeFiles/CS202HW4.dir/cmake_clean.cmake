5f02cba1ba6b355314b5198bfab3b4fa0452b7b8
CSWMandrews <wmandrews@alaska.edu>
2019-10-24 16:42:35 -0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/CS202HW4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS202HW4.dir/main.cpp.obj"
+  "CS202HW4.pdb"
+  "CS202HW4.exe"
+  "CS202HW4.exe.manifest"
+  "libCS202HW4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CS202HW4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CS202HW4.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CS202HW4.
+# This may be replaced when dependencies are built.
